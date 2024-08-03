8d31ac3f3d3c0f1d635a3c247a472d7b907ecfbe
Naufal Akbar <naufalakbar378@gmail.com>
2024-08-03 17:22:00 +0700

    feat: replace Overview component with ContainerOverview in Admin section

---
+++ src/components/Containers/Admin/Overview/Overview.tsx
@@ -0,0 +1,41 @@
+'use client';
+import { FC, useCallback, useEffect } from 'react';
+import { useTranslations } from 'next-intl';
+import { redirect } from 'next/navigation';
+
+import { useAxios } from '@/hooks/useAxios';
+import { useAuth } from '@/hooks/useAuth';
+
+const ContainerOverview: FC = () => {
+  // Define translations
+  const t = useTranslations('HomePage');
+
+  const { accessToken, user } = useAuth();
+  const axios = useAxios(accessToken);
+
+  const getTicket = useCallback(async () => {
+    try {
+      const response = await axios.get('ticket/graph');
+      console.log(response.data);
+    } catch (error) {
+      console.error('GET_TICKET_ERROR', error);
+    }
+  }, [axios]);
+
+  useEffect(() => {
+    getTicket();
+  }, [getTicket]);
+
+  if (user?.role !== 'admin') {
+    return redirect('/tickets');
+  }
+
+  return (
+    <section className="max-w-7xl mx-auto">
+      {t('title')}
+      <h1>awdawda</h1>
+    </section>
+  );
+};
+
+export default ContainerOverview;
