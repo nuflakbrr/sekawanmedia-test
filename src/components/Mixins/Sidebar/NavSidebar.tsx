5b30e78116b5e2df028a96dfa88af1c46166bc3b
Naufal Akbar <naufalakbar378@gmail.com>
2024-08-03 18:54:29 +0700

    feat: Update NavSidebar to display page title dynamically
    The NavSidebar component has been updated to display the page title dynamically based on the current route. This change improves the user experience by providing clear and relevant page titles in the sidebar, making it easier for users to navigate the application.
    
    Co-authored-by: [Author Name]

--- src/components/Mixins/Sidebar/NavSidebar.tsx
+++ src/components/Mixins/Sidebar/NavSidebar.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 'use client';
 import { FC } from 'react';
 import { useTranslations } from 'next-intl';
@@ -10,13 +11,29 @@ import {
   TooltipTrigger,
 } from '@/components/ui/tooltip';
 import MobileSidebar from './MobileSidebar';
+import { usePathname } from 'next/navigation';
 
 const NavSidebar: FC = () => {
   const t = useTranslations('Tooltip');
 
+  const pathname = usePathname();
+  const isAdminPage = pathname?.startsWith('/admin');
+
+  const titlePageAdmin = ['overview', 'tickets', 'settings'];
+  const titlePageGuest = ['tickets', 'settings'];
+
+  const titlePage = isAdminPage
+    ? titlePageAdmin.find(
+      (title) => pathname === `/admin/${title}` || pathname === '/admin',
+    )
+    : titlePageGuest.find((title) => pathname === `/${title}` || pathname === '/');
+
   return (
     <div className="p-4 border-b h-16 flex items-center bg-white dark:bg-[#020817] shadow-sm">
       <MobileSidebar />
+      <h1 className="text-xl font-bold text-[#1E293B] dark:text-white capitalize">
+        {titlePage}
+      </h1>
       <div className="flex items-center ml-auto space-x-4">
         <Tooltip>
           <TooltipTrigger>
