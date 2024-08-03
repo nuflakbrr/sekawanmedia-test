6251a59f01d9107cd8a4d5fd191ad3d42d2843ca
Naufal Akbar <naufalakbar378@gmail.com>
2024-08-03 17:12:35 +0700

    feat: add admin page and ticket page

---
+++ src/app/(root)/(routes)/admin/page.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+import ContainerOverview from '@/components/Containers/Overview/Overview';
+
+const Overview: FC = () => {
+  return <ContainerOverview />;
+};
+
+export default Overview;
