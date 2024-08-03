2715611ebdf26508b5c6ec1cc801c1b9877ee8e6
Naufal Akbar <naufalakbar378@gmail.com>
2024-08-03 18:12:29 +0700

    feat: Add settings page component
    This commit adds the `SettingPage` component to the project. It imports the `ContainerSettings` component from the `@/components/Containers/Settings/Settings` module and renders it. This component will be used for displaying and managing settings in the application.
    
    Co-authored-by: [Author Name]

---
+++ src/app/(root)/(routes)/admin/settings/page.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+import ContainerSettings from '@/components/Containers/Settings/Settings';
+
+const SettingPage: FC = () => {
+  return <ContainerSettings />;
+};
+
+export default SettingPage;
